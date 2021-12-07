913e51840b2dbcf2b1e492b5d8fcb932ff5402d1
Sonmusic <64028850+Sonmusic@users.noreply.github.com>
2021-12-06 21:53:27 -0400

    Magnitude y Constructor + TDD

--- VectorsTests/UnitTest1.cs
+++ VectorsTests/UnitTest1.cs
@@ -8,9 +8,17 @@ namespace VectorsTests
         [TestMethod]
         public void Return_True_Magnitude_Vector12() 
         {
-            VectorsAndAngles.Vector vector = VectorsAndAngles.Vector();
-            double magn = vector.Magnitude();
-            Assert.AreEqual(magn, x);
+            VectorsAndAngles.Vector vector = new VectorsAndAngles.Vector(1, 2);
+            double magn = vector.magnitude();
+            Assert.AreEqual(magn, 2.2360679774997896964091736687313);
+        }
+
+        [TestMethod]
+        public void Return_False_Magnitude_Vector12()
+        {
+            VectorsAndAngles.Vector vector = new VectorsAndAngles.Vector(1, 2);
+            double magn = vector.magnitude();
+            Assert.AreNotEqual(magn, 2.25);
         }
     }
 }
